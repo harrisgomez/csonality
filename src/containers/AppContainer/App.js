6c35e5ada6279027cdc6996dd1486a292a5c821e
Harris Gomez <harrisgomez28@gmail.com>
2019-10-22 10:48:31 -0400

    Created project hierarchy; updated import paths

---
+++ src/containers/AppContainer/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Project hierarchy created.</h1>
+      <h1>All paths updated.</h1>
+      <h1>Let's build some components!</h1>
+    </div>
+  );
+}
+
+export default App;
