2f895b741e0e5f221f0a5e6ba3fd019e9780dc80
Harris Gomez <harrisgomez28@gmail.com>
2020-01-03 06:06:50 -0500

    Created basic layout from wireframe. Included title and card sections, calendar buttons, and Add Card button.

--- src/containers/AppContainer/App.js
+++ src/containers/AppContainer/App.js
@@ -3,13 +3,38 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/App.css';
 
+import { Card, Button } from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
-      <div className='header'>CSONALITY</div>
-      <div className='container'>
-        <div className='watchlist'>WATCHLIST</div>
-        <div className='graph'>GRAPH</div>
+      <div className='header'>
+        <Card style={{ width: '34rem' }}>
+          <Card.Body>
+            <Card.Title>CSONALITY</Card.Title>
+            <Card.Text style={{ fontSize: '14px' }}>
+              <p>Csonality is a study that attempts to capture trending periods in the stock market and offers a simple probability analysis based on historical data.</p>
+              <p>Traders can combine this with their favorite trading strategy to ideally profit through informed decisions.</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className='main'>
+        <div className='card'>
+          <div className="card-input">
+            <input type="text" placeholder='Symbol search...' />
+          </div>
+          <div className="card-calendar">CALENDAR</div>
+          <div className="timeframes">
+            <button>D</button>
+            <button>W</button>
+            <button>M</button>
+            <button>Q</button>
+          </div>
+        </div>
+        <button className="add-card-btn">
+          <span>+ Add Card</span>
+        </button>
       </div>
     </div>
   );
